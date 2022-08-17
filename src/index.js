05fc26ed4ac31bbdc441736612a1743173fc0699
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-08-17 13:09:42 -0700

    mustafa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 // write your code here
+// Global
+
+const ramenMenu =document.querySelector("#ramen-menu");
+const baseUrl = "http://localhost:3000";
+
+
+//Fetchers
+fetch(baseUrl + "/ramens")
+ .then((res) => res.json())
+ .then((ramensData) => renderRamens (ramensData));
+
+
+ // render Functions
+
+ function renderRamens(ramens){
+   ramens.forEach((ramen) => {
+     const img = document.createElement("img");
+     img.addEventListener('click',updateRamen)
+     img.details=ramen 
+     img.src = ramen.image;
+    ramenMenu.append (img);
+   });
+ }
+
+function updateRamen(e){
+  let ramen= e.target.details;
+  const name= document.querySelector(".name");
+  name.innertext=ramen.name;
+  const image = document.querySelector(".detail-image");
+  image.src = ramen.image;
+  const restaurant = document.querySelector(".restaurant");
+  restaurant.innertext= ramen.restaurant;
+  const rating=  document.querySelector("#rating-display");
+  rating.innertext=ramen.rating;
+  const comment = document.querySelector("#comment-display");
+  comment.innertext = ramen.comment;
+} 
+
+
+const ramenForm = document.querySelector("#new-ramen") 
+ramenForm.addEventListener ("submit", createRamen)
+ 
+// New Form
+
+function createRamen(event){
+  event.prevent.Default();
+  const name = document.querySelector ("#new-name".value);
+  const restaurant = document.querySelector ("#new-restaurant").value;
+  const image = document.querySelector("#new-image").value;
+  const rating =document.querySelector("#new-rating").value;
+  const comment =document.querySelector("#new-comment").value;
+  console.log(name,restaurant,image,rating,comment); 
+  appendRamenImage(ramen);
+
+
+ }
---
+++ src/notes.md
@@ -0,0 +1,21 @@
+ # Core deliverables 
+challenge 1
+As a User,
+ 1. select #ramen-menu div
+ 2. fetch ramens data ,and for each ramen create an image
+ 3. iterate over ramen data and use for Each
+ * create img tag
+ * img src 
+ * append to ramen div 
+
+ challlenge 2
+
+ 1. add event listener to each image in the #ramen menu div
+ 2. callback function that will update ramen detial 
+ 3. select name ,restaurant,image,rating and comment tags in ramen-detail div 
+
+challenge 3
+
+1. add a submit eventlistener to the new ramen form 
+2. callback function will add the ramen to ramen-menu div
+3. select all inputs field from form
